935e714ba80e851f1f8295a64afc6a4c8a7a03c2
Dana Aldulijan <daldulijan@hotmail.com>
2025-06-02 09:28:29 +0300

    added new user

--- IdentityPifss/Controllers/AccountController.cs
+++ IdentityPifss/Controllers/AccountController.cs
@@ -18,17 +18,30 @@ namespace IdentityPifss.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Register(RegisterViewModel model)
+        public async Task<IActionResult> Register(RegisterViewModel model)
         {
-            if (ModelState.IsValid) {
+            if (ModelState.IsValid)
+            {
                 IdentityUser user = new IdentityUser
                 {
                     UserName = model.Email,
                     Email = model.Email,
                     PhoneNumber = model.Mobile
                 };
-
-            return View();
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    // Optionally, you can sign in the user after registration
+                    // await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Login");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+                return View(model);
+            }
+            return View(model);
         }
     }
 }
