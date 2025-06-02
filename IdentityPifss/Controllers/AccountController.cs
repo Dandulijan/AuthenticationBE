3694ca47ce8dbe6a7192b87e9ded825c0a9d61d4
Dana Aldulijan <daldulijan@hotmail.com>
2025-06-02 08:58:07 +0300

    Add project files.

---
+++ IdentityPifss/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using IdentityPifss.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityPifss.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<IdentityUser> _userManager;
+        public AccountController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid) {
+                IdentityUser user = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    PhoneNumber = model.Mobile
+                };
+
+            return View();
+        }
+    }
+}
---
+++ IdentityPifss/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityPifss.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+        public string Mobile { get; set; }    
+    }
+}
